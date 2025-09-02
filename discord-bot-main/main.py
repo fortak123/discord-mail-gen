480c85b96b81fcb59c2e8a7466f96ff6600906bb
fortak123 <113169567+fortak123@users.noreply.github.com>
2025-09-02 14:55:39 +0200

    Update main.py

--- discord-bot-main/main.py
+++ discord-bot-main/main.py
@@ -15,7 +15,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 # Role & cooldowny
 NORMAL_ROLE = "normal"
